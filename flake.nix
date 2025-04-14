99caf4cece6d19ac67fdfe4507bbab9f61528986
Sam Estep <sam@samestep.com>
2025-04-14 09:19:45 -0400

    Add some scaffolding

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+      in
+      {
+        devShell =
+          with pkgs;
+          mkShell {
+            buildInputs = [
+              clang-tools
+              cudatoolkit
+              nixfmt-rfc-style
+            ];
+          };
+      }
+    );
+}
